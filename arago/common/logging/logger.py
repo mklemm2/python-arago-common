e185749ded8b27424af16e2d5970724364b33847
Marcus Klemm <mklemm@arago.de>
2019-06-27 15:51:21 +0200

    Add HEARTBEAT level

--- arago/common/logging/logger.py
+++ arago/common/logging/logger.py
@@ -3,6 +3,7 @@ import logging
 class Logger(logging.getLoggerClass()):
 	CRITICAL=50
 	ERROR=40
+	HEARTBEAT=35
 	WARNING=30
 	INFO=20
 	VERBOSE=15
@@ -13,9 +14,13 @@ class Logger(logging.getLoggerClass()):
 		super().__init__(name, level)
 		logging.addLevelName(self.VERBOSE, "VERBOSE")
 		logging.addLevelName(self.TRACE, "TRACE")
+		logging.addLevelName(self.HEARTBEAT, "HEARTBEAT")
 	def verbose(self, msg, *args, **kwargs):
 		if self.isEnabledFor(self.VERBOSE):
 			self._log(self.VERBOSE, msg, args, **kwargs)
 	def trace(self, msg, *args, **kwargs):
 		if self.isEnabledFor(self.TRACE):
 			self._log(self.TRACE, msg, args, **kwargs)
+	def heartbeat(self, msg, *args, **kwargs):
+		if self.isEnabledFor(self.HEARTBEAT):
+			self._log(self.HEARTBEAT, msg, args, **kwargs)
