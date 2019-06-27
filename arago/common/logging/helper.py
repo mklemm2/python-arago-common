d7d906e065c5e0c525e8dc26057506dfa79d2ad2
Marcus Klemm <mklemm@arago.de>
2019-06-27 13:25:22 +0200

    Unified handling of logfile and stderr

--- arago/common/logging/helper.py
+++ arago/common/logging/helper.py
@@ -1,24 +1,23 @@
 import logging, sys
 from arago.common.logging.logger import Logger
 
-def getCustomLogger(level="INFO", logfile=None, formatting=None):
+def getCustomLogger(level="INFO", logfile=sys.stderr, formatting=None):
 	logging.setLoggerClass(Logger)
 	logger = logging.getLogger('root')
 	level = getattr(logger, level)
 	logger.setLevel(level)
-	if logfile:
-		if formatting:
-			formatter = logging.Formatter(*formatting)
-		else:
-			formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(module)s - %(message)s", "%Y-%m-%d %H:%M:%S")
-		if logfile != sys.stderr:
-			handler = logging.FileHandler(logfile)
-		elif logfile == sys.stderr:
-			handler = logging.StreamHandler()
-		handler.setFormatter(formatter)
-		handler.setLevel(level)
-		for h in list(logger.handlers):
-			if h.stream.name == handler.stream.name:
-				logger.removeHandler(h)
-		logger.addHandler(handler)
+	if formatting:
+		formatter = logging.Formatter(*formatting)
+	else:
+		formatter = logging.Formatter("%(asctime)s %(levelname)-7s %(message)s", "%Y-%m-%d %H:%M:%S")
+	if logfile != sys.stderr:
+		handler = logging.FileHandler(logfile)
+	elif logfile == sys.stderr:
+		handler = logging.StreamHandler()
+	handler.setFormatter(formatter)
+	handler.setLevel(level)
+	for h in list(logger.handlers):
+		if h.stream.name == handler.stream.name:
+			logger.removeHandler(h)
+	logger.addHandler(handler)
 	return logger
