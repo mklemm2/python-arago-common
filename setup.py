c4b16792ba3d587a8624471d4065379a51b91c8e
Marcus Klemm <mklemm@arago.de>
2019-11-08 10:20:03 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import distutils.core
 
 distutils.core.setup(
 	name = "arago-common-base",
-	version = "2.2",
+	version = "2.3",
 	author = "Marcus Klemm",
 	author_email = "mklemm@arago.de",
 	description = ("Common functions and classes"),
